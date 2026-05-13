a2149ad894ef1897bb55947a8a2e665c69de0c77
sbdream8 <sebindream@gmail.com>
2026-05-13 15:53:27 +0900

    fix forms

--- project/models/resnet.py
+++ project/models/resnet.py
@@ -4,15 +4,8 @@ import torch.nn as nn
 def ResNet18():
     model = resnet18(num_classes=10)
 
-    model.conv1 = nn.Conv2d(
-        3, 64,
-        kernel_size=3,
-        stride=1,
-        padding=1,
-        bias=False
-    )
+    model.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
 
     model.maxpool = nn.Identity()
 
     return model
-
