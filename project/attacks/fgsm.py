Correct FGSM/PGD perturbation scale under input normalization

project/attacks/fgsm.py
@@ -10,11 +10,11 @@ def fgsm_attack(model, images, labels, epsilon, device):
     loss = F.cross_entropy(outputs, labels)
     model.zero_grad()
     loss.backward()
-    adv_images = (images + epsilon * images.grad.sign())
+    # adv_images = (images + epsilon * images.grad.sign())
+    std = torch.tensor([0.2470, 0.2435, 0.2616], device=device).view(1,3,1,1)
+    eps = epsilon / std
+    adv_images = images + eps * images.grad.sign()
     adv_images = torch.clamp(adv_images, 0, 1)
-
-    print(images.min(), images.max())
-
 
     return adv_images.detach()
 

project/attacks/pgd.py
@@ -3,6 +3,10 @@ import torch.nn.functional as F
 
 def pgd_attack(model, images, labels, epsilon, alpha, iters, device):
 
+    std = torch.tensor([0.2470, 0.2435, 0.2616], device=device).view(1,3,1,1)
+    eps = epsilon / std
+    alp = alpha / std
+
     images = images.clone().detach().to(device)
     labels = labels.to(device)
     ori_images = images.clone().detach()
@@ -12,10 +16,13 @@ def pgd_attack(model, images, labels, epsilon, alpha, iters, device):
         images.requires_grad = True
         outputs = model(images)
         loss = F.cross_entropy(outputs, labels)
+        grad = torch.autograd.grad( loss, images, retain_graph=False, create_graph=False )[0]
         model.zero_grad()
         loss.backward()
         adv_images = (images + alpha * images.grad.sign())
-        eta = torch.clamp(adv_images - ori_images, min=-epsilon, max=epsilon)
-        images = torch.clamp(ori_images + eta, 0, 1).detach()
+        # eta = torch.clamp(adv_images - ori_images, min=-epsilon, max=epsilon) ->
+        eta = torch.max( torch.min( adv_images - ori_images, eps ), -eps )
+        # images = torch.clamp(ori_images + eta, 0, 1).detach() ->
+        images = (ori_images + eta).detach()
 
     return images

project/pgd.py
@@ -1,96 +1,96 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.transforms as transforms
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torchvision.transforms as transforms
 
-from models.resnet import ResNet18
-from models.cnn import SimpleCNN
+# from models.resnet import ResNet18
+# from models.cnn import SimpleCNN
 
-from torchvision import datasets
-from torch.utils.data import DataLoader
+# from torchvision import datasets
+# from torch.utils.data import DataLoader
 
-cifar10_mean = (0.4914, 0.4822, 0.4465)
-cifar10_std = (0.2470, 0.2435, 0.2616)
-transform_test = transforms.Compose([ transforms.ToTensor(), transforms.Normalize(cifar10_mean, cifar10_std) ])
+# cifar10_mean = (0.4914, 0.4822, 0.4465)
+# cifar10_std = (0.2470, 0.2435, 0.2616)
+# transform_test = transforms.Compose([ transforms.ToTensor(), transforms.Normalize(cifar10_mean, cifar10_std) ])
 
-test_dataset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
-test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
+# test_dataset = datasets.CIFAR10(root="./data", train=False, download=True, transform=transform_test)
+# test_loader = DataLoader(test_dataset, batch_size=128, shuffle=False)
 
-use_cuda = True
-device = torch.device("cuda" if use_cuda else "cpu")
-CHECKPOINT_DIR = "/content/drive/MyDrive/adversarial_checkpoints"
-cnn = SimpleCNN().to(device)
-cnn.load_state_dict(torch.load(f"{CHECKPOINT_DIR}/cnn_best.pth", map_location=device))
+# use_cuda = True
+# device = torch.device("cuda" if use_cuda else "cpu")
+# CHECKPOINT_DIR = "/content/drive/MyDrive/adversarial_checkpoints"
+# cnn = SimpleCNN().to(device)
+# cnn.load_state_dict(torch.load(f"{CHECKPOINT_DIR}/cnn_best.pth", map_location=device))
 
-resnet = ResNet18().to(device)
-resnet.load_state_dict(torch.load(f"{CHECKPOINT_DIR}/resnet18_best.pth", map_location=device))
+# resnet = ResNet18().to(device)
+# resnet.load_state_dict(torch.load(f"{CHECKPOINT_DIR}/resnet18_best.pth", map_location=device))
 
-def evaluate_clean(model, test_loader, device):
+# def evaluate_clean(model, test_loader, device):
 
-    model.eval()
-    correct = 0
-    total = 0
+#     model.eval()
+#     correct = 0
+#     total = 0
 
-    with torch.no_grad():
+#     with torch.no_grad():
 
-        for images, labels in test_loader:
+#         for images, labels in test_loader:
 
-            images = images.to(device)
-            labels = labels.to(device)
-            outputs = model(images)
-            pred = outputs.argmax(1)
-            correct += pred.eq(labels).sum().item()
-            total += labels.size(0)
+#             images = images.to(device)
+#             labels = labels.to(device)
+#             outputs = model(images)
+#             pred = outputs.argmax(1)
+#             correct += pred.eq(labels).sum().item()
+#             total += labels.size(0)
 
-    return 100 * correct / total
+#     return 100 * correct / total
 
-def pgd_attack(model, images, labels, epsilon=8/255, alpha=2/255, num_steps=10, device="cuda"):
+# def pgd_attack(model, images, labels, epsilon=8/255, alpha=2/255, num_steps=10, device="cuda"):
 
-    images = images.clone().detach().to(device)
-    labels = labels.to(device)
+#     images = images.clone().detach().to(device)
+#     labels = labels.to(device)
 
-    adv_images = images + torch.empty_like(images).uniform_(-epsilon, epsilon)
+#     adv_images = images + torch.empty_like(images).uniform_(-epsilon, epsilon)
 
-    for _ in range(num_steps):
+#     for _ in range(num_steps):
 
-        adv_images.requires_grad = True
-        outputs = model(adv_images)
-        loss = F.cross_entropy(outputs, labels)
-        grad = torch.autograd.grad(loss, adv_images, retain_graph=False, create_graph=False)[0]
-        adv_images = adv_images.detach() + alpha * grad.sign()
+#         adv_images.requires_grad = True
+#         outputs = model(adv_images)
+#         loss = F.cross_entropy(outputs, labels)
+#         grad = torch.autograd.grad(loss, adv_images, retain_graph=False, create_graph=False)[0]
+#         adv_images = adv_images.detach() + alpha * grad.sign()
 
-        # Projection
-        delta = torch.clamp(adv_images - images, min=-epsilon, max=epsilon)
-        adv_images = (images + delta).detach()        
+#         # Projection
+#         delta = torch.clamp(adv_images - images, min=-epsilon, max=epsilon)
+#         adv_images = (images + delta).detach()        
 
-    return adv_images
+#     return adv_images
 
 
-def evaluate_pgd(model, test_loader, device):
+# def evaluate_pgd(model, test_loader, device):
 
-    model.eval()
-    correct = 0
-    total = 0
+#     model.eval()
+#     correct = 0
+#     total = 0
 
-    for images, labels in test_loader:
+#     for images, labels in test_loader:
 
-        images = images.to(device)
-        labels = labels.to(device)
+#         images = images.to(device)
+#         labels = labels.to(device)
 
-        adv_images = pgd_attack(model, images, labels, epsilon=8/255, alpha=2/255, num_steps=10, device=device)
-        outputs = model(adv_images)
-        pred = outputs.argmax(1)
-        correct += pred.eq(labels).sum().item()
-        total += labels.size(0)
+#         adv_images = pgd_attack(model, images, labels, epsilon=8/255, alpha=2/255, num_steps=10, device=device)
+#         outputs = model(adv_images)
+#         pred = outputs.argmax(1)
+#         correct += pred.eq(labels).sum().item()
+#         total += labels.size(0)
 
-    return 100 * correct / total
+#     return 100 * correct / total
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
 
-    print("Loading CIFAR10...")
+#     print("Loading CIFAR10...")
 
-    clean_acc = evaluate_clean(cnn, test_loader, device)
-    pgd_acc = evaluate_pgd(cnn, test_loader, device)
+#     clean_acc = evaluate_clean(cnn, test_loader, device)
+#     pgd_acc = evaluate_pgd(cnn, test_loader, device)
 
-    print(f"CNN Clean Acc : {clean_acc:.2f}%")
-    print(f"CNN PGD Acc   : {pgd_acc:.2f}%")
+#     print(f"CNN Clean Acc : {clean_acc:.2f}%")
+#     print(f"CNN PGD Acc   : {pgd_acc:.2f}%")